dbc848d7d2a48477bf3a6c47ae92074a4b40f6c4
Xavier Crespo <xavier.crespo1@gmail.com>
2019-10-08 09:35:05 +0200

    First Commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home(){
+  return(
+    <div>
+      <h1>Home</h1>
+      <p>Hello, my name is Xavier and you are on my solution fotr the Ironhack Challenge!</p>
+    </div>
+  )
+}
